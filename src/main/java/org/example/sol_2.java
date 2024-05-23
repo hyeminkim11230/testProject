f9e58236c1d101213eefe71f5bbe4742294f3e1e
DESKTOP-7NI8BH3\HYEMIN <kimhyemin11230@gmail.com>
2024-05-24 01:04:49 +0900

    feat 두 개 뽑아서 더하기

--- src/main/java/org/example/sol_2.java
+++ src/main/java/org/example/sol_2.java
@@ -1,8 +1,22 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.HashSet;
+
 public class sol_2 {
     public static void main(String[] args) {
+        int[] arr= {5,1,3,4,2,2,7};
+        System.out.println(Arrays.toString(solution2(arr)));
+    }
 
-        System.out.println("Hello world!");
+    public static int[] solution2(int[] numbers){
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < numbers.length - 1; i++) {
+            for (int j = i+1; j < numbers.length; j++) {
+                set.add(numbers[i]+numbers[j]);
+            }
+        }
+        return set.stream().sorted().mapToInt(Integer::intValue).toArray();
     }
 }
