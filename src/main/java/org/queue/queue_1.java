ecf0d5cab8057fcea7ba4f113e9e274733ddb733
DESKTOP-7NI8BH3\HYEMIN <kimhyemin11230@gmail.com>
2024-06-11 22:55:18 +0900

    feat queue 기능 개발

--- src/main/java/org/queue/queue_1.java
+++ src/main/java/org/queue/queue_1.java
@@ -1,27 +1,43 @@
 package org.queue;
 
 import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
+//기능 개발
 public class queue_1 {
     public static void main(String[] args) {
-
+        int[] progresses = {93,30,55};
+        int[] speeds = {1,30,5};
+        System.out.println(Arrays.toString(solution(progresses, speeds))); ;
     }
-    private int solution(int N,int K) {
-        //1부터 N까지의 번호를 deque에 추가
-        ArrayDeque<Integer> deque = new ArrayDeque<>();
-        for (int i = 1; i <= N; i++) {
-            deque.addLast(i);
+    private static int[] solution(int[] progresses, int[] speeds) {
+        ArrayDeque<Integer> answer = new ArrayDeque<>();
+
+        int n = progresses.length;
+        //각 작업의 배포가능일 계산
+        int[] daysLeft = new int[n];
+        for (int i = 0; i < n; i++) {
+            //Math.ceil() 소수점을 올림 연산하는 메서드
+            daysLeft[i] =(int) Math.ceil((100.0 - progresses[i])/speeds[i]);
         }
 
-        //deque에 하나의 요소가 남을 때까지 반복
-        while (deque.size() > 1) {
-            //K번째 요소를 찾기 위해 앞에서부터 제거하고 뒤에 추가
-            for (int i = 0; i < K - 1; i++) {
-                deque.addLast(deque.pollFirst());
+        int count = 0; //배포될 작업의 수 카운트
+        int maxDay = daysLeft[0]; // 현재 배포될 작업 중 가장 늦게 배포될 작업의 가능일
+
+        for (int i = 0; i < n; i++) {
+            if (daysLeft[i] <= maxDay) { //배포 가능일이 가장 늦은 배포일보다 빠르면
+                count++;
             }
-            deque.pollFirst(); //K번째 요소 제거
+            else{ // 배포 예정일이 기준 배포일보다 늦으면
+                answer.add(count);
+                count=1;
+                maxDay = daysLeft[i];
+            }
         }
 
-        return deque.pollFirst(); //마지막으로 남은 요소 반환
+        answer.add(count); //마지막으로 카운트된 작업들을 함께 배포
+        return answer.stream().mapToInt(Integer::intValue).toArray();
     }
 }
